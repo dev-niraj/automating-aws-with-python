666af2b5a8dd02c5f50831771671bbe65728c3d5
dev-niraj <think2secure@gmail.com>
2019-11-27 08:08:29 -0800

    Configure DNS with Route53

--- automate-aws/app.py
+++ automate-aws/app.py
@@ -16,19 +16,21 @@ import boto3
 import click
 
 from bucket import BucketManager
-
+from domain import DomainManager
+import util
 
 # s3 = session.resource('s3')
 
 session = None
 bucket_manager = None
+domain_manager = None
 
 
 @click.group()
 @click.option('--profile', default=None, help="Use a given AWS profile.")
 def cli(profile):
     """Script deployes websites to AWS."""
-    global session, bucket_manager
+    global session, bucket_manager, domain_manager
 
     session_cfg = {}
     if profile:
@@ -36,6 +38,7 @@ def cli(profile):
 
     session = boto3.Session(**session_cfg)
     bucket_manager = BucketManager(session)
+    domain_manager = DomainManager(session)
 
 
 @cli.command('list-buckets')
@@ -72,6 +75,19 @@ def sync(pathname, bucket):
     bucket_manager.sync(pathname, bucket)
     print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
 
+
+@cli.command('setup-domain')
+@click.argument('domain')
+def setup_domain(domain):
+    """Configure DOMAIN to point to BUCKET."""
+    bucket = bucket_manager.get_bucket(domain)
+
+    zone = domain_manager.find_hosted_zone(domain) \
+        or domain_manager.create_hosted_zone(domain)
+
+    endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
+    domain_manager.create_s3_domain_record(zone, domain, endpoint)
+    print("Domain configured: http://{}".format(domain))
 
 
 if __name__ == "__main__":
